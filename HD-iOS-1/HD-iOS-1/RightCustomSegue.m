7001ea93dff65b721ee289826aec5d01cc973aa0
Tor is Here <kittikorn.a@gmail.com>
2015-07-04 13:38:57 +0900

    Custom segue animation

---
+++ HD-iOS-1/HD-iOS-1/RightCustomSegue.m
@@ -0,0 +1,13 @@
+//
+//  RightCustomSegue.m
+//  HD-iOS-1
+//
+//  Created by Kittikorn Ariyasuk on 7/4/15.
+//  Copyright (c) 2015 TorIsHere. All rights reserved.
+//
+
+#import "RightCustomSegue.h"
+
+@implementation RightCustomSegue
+
+@end
---
+++ HD-iOS-1/HD-iOS-1/RightCustomSegueUnwind.m
@@ -0,0 +1,13 @@
+//
+//  RightCustomSegueUnwind.m
+//  HD-iOS-1
+//
+//  Created by Kittikorn Ariyasuk on 7/4/15.
+//  Copyright (c) 2015 TorIsHere. All rights reserved.
+//
+
+#import "RightCustomSegueUnwind.h"
+
+@implementation RightCustomSegueUnwind
+
+@end
