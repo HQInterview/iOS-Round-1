b059e187580e2739978d336618b437b1040da26f
jianyaoang <jianyaoang1@gmail.com>
2016-01-17 14:27:59 +0800

    Create project file, made corrections to json file

---
+++ HQ iOS Test Project/HotelQuicklyRound1/HotelQuicklyRound1/AppDelegate.m
@@ -0,0 +1,68 @@
+//
+//  AppDelegate.m
+//  HotelQuicklyRound1
+//
+//  Created by Ang Jian Yao on 17/01/2016.
+//  Copyright © 2016 Ang Jian Yao. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    
+//    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"country_data" ofType:@"json"];
+//    NSError *error;
+//    NSData *filePathData = [[NSData alloc] initWithContentsOfFile:filePath];
+//    
+//    NSDictionary *jsonDictionary = [NSJSONSerialization JSONObjectWithData:filePathData options:NSJSONReadingAllowFragments error:&error];
+//    NSLog(@"jsonDictionary %@", jsonDictionary);
+
+    
+//    NSString *fileContents = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:&error];
+
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"country_data" ofType:@"json"];
+    NSString *fileContents = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+    
+    NSData *jsonData = [fileContents dataUsingEncoding:NSUTF8StringEncoding];
+                                      
+    NSError *jsonError;
+    NSMutableDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingAllowFragments error:&jsonError];
+    NSArray *countries = [jsonDict valueForKey:@"countries"];
+
+    
+    NSLog(@"jsonDict %@", countries);
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
---
+++ HQ iOS Test Project/HotelQuicklyRound1/HotelQuicklyRound1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HotelQuicklyRound1
+//
+//  Created by Ang Jian Yao on 17/01/2016.
+//  Copyright © 2016 Ang Jian Yao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
